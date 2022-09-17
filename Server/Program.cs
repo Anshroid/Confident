5400d0041889e6e64ff75820d6cf2586acc3b6c6
Anshroid Nemo <anshroid07@gmail.com>
2022-09-17 13:19:08 +0100

    Initialise

---
+++ Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8023);
+            
+            server.Start();
+            Console.WriteLine("Server has started on 127.0.0.1:8023{0}Waiting for a connection...", Environment.NewLine);
+
+            var client = server.AcceptTcpClient();
+            
+            Console.WriteLine("A client connected.");
+        }
+    }
+}
