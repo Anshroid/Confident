bd9699bed354f32b5da1dd77ab5af1bc89ec7d42
Anshroid Nemo <anshroid07@gmail.com>
2022-09-19 15:39:58 +0100

    Add Packet.cs structure and some basic packets + Formatting

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
-using System.Net.Sockets;
+using Server.Packets;
+using WebSocketSharp.Server;
 
 namespace Server
 {
     internal class Program
     {
+        public static readonly Dictionary<string, ConfidentClient> ClientList = new Dictionary<string, ConfidentClient>();
         public static void Main(string[] args)
         {
-            var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8023);
+            var server = new WebSocketServer(IPAddress.Parse("127.0.0.1"), 8023);
             
-            server.Start();
-            Console.WriteLine("Server has started on 127.0.0.1:8023{0}Waiting for a connection...", Environment.NewLine);
+            Packet.RegisterAll();
 
-            var client = server.AcceptTcpClient();
-            
-            Console.WriteLine("A client connected.");
+            server.AddWebSocketService<ConfidentClient>("/");
+            server.KeepClean = false;
+            server.Start();
+            Console.ReadKey(true);
         }
     }
 }
